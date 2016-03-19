74991ef6caa7d20650f710d8f253d2a814439c0b
jerelynco <jerelynco@gmail.com>
2016-03-19 13:49:02 +0800

    All + w/o gyroscope models

--- algo_training/prediction/predictor.py
+++ algo_training/prediction/predictor.py
@@ -9,7 +9,8 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.externals import joblib
 import string
-import serial, serial.tools.list_ports
+import serial
+import serial.tools.list_ports
 import numpy as np
 
 
@@ -20,14 +21,14 @@ predictors = {
 }
 
 # Arduino
-serialPort = "/dev/ttyACM2" 
+serialPort = "/dev/ttyACM2"
 BAUD_RATE = 9600
 
 # "Group"
-directory = "all/" # Change this if needed
+directory = "all/"  # Change this if needed
 
 # File names
-classifier = "RandomForestClassifier.pkl" # Change this if needed
+classifier_fn = "RandomForestClassifier.pkl"  # Change this if needed
 
 scaler_fn = "scaler.pkl"
 
@@ -43,22 +44,15 @@ target_names = [i for i in string.ascii_uppercase]
 ser = serial.Serial(serialPort, BAUD_RATE)
 
 # Loading scaler
-scaler = joblib.load(dir_scaler+directory+scaler_fn)
-predictor = joblib.load(dir_classifiers+directory+)
+scaler = joblib.load(dir_scaler + directory + scaler_fn)
+predictor = joblib.load(dir_classifiers + directory + classifier_fn)
 
 while(1):
-	row = ser.readline().decode("utf-8").strip().split(",")
-	int_row = np.array([int(val) for val in row])
+    row = ser.readline().decode("utf-8").strip().split(",")
+    int_row = np.array([int(val) for val in row])
 
-	if directory == "flex_only":
-		int_row = int_row[:5] 
-	elif directory == "wo_ag":
-		int_row = int_row[:-6] 
-		
-	prediction = predictor.predict(scaler.transform(int_row.reshape(1,-1)))
-	print("%s: %s" % (row, target_names[prediction]))
+    if directory == "wo_g":
+        int_row = int_row[:-3]
 
-
-
-
-
+    prediction = predictor.predict(scaler.transform(int_row.reshape(1, -1)))
+    print("%s: %s" % (row, target_names[prediction]))
